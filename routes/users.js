399634882ce6660e47a8b2f01c7057bb9df7f700
Rohan Antony <rohan271995@gmail.com>
2015-10-19 11:07:54 +0530

    static files like bootstrap css and angular js handled

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/user1',function(req,res){
+  res.write('This is me!!');
+})
+
 module.exports = router;
