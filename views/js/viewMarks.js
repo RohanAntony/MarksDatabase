e99d67244962a76d93df6c65a0a2427c2786d21d
Rohan Antony <rohan271995@gmail.com>
2015-11-02 23:40:52 +0530

    end of development

--- views/js/viewMarks.js
+++ views/js/viewMarks.js
@@ -6,7 +6,7 @@ app.controller('viewMarks',function($scope,$http){
     }).then(function(success){
       console.log(success.data);
       $scope.name = success.data[0].name;
-      $scope.details = success.data;
+      $scope.details = success.data;  //store the details of the marks in $scope.details
     },function(error){
       $scope.alertText = "Error in getting GROUP subjects @ angularJS";
     });
@@ -15,7 +15,26 @@ app.controller('viewMarks',function($scope,$http){
   $scope.showDetails = function(){
     $scope.detailRecord = $scope.details[$scope.recordDetails];
     console.log($scope.detailRecord);
+    $scope.detailRecord.marks.forEach(function(subject,index){
+      subject.external.forEach(function(subjectInner,indexInner){
+
+      });
+    });
     // console.log($scope.recordDetails);
+  }
+
+  $scope.returnWord = function(value){
+    switch(value){
+      case '0':
+        return "First Attempt";
+        break;
+      case '1':
+        return "Second Attempt";
+        break;
+      case '2':
+        return "Third Attempt";
+        break;
+    }
   }
 
 })
